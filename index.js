2af73909557ad331ca193e0d7f4b68c24cc59465
hasintha <hasinthadarshana3@gmail.com>
2025-04-07 18:16:42 +0530

    initializing the project

---
+++ index.js
@@ -0,0 +1,57 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+import studentRouter from './routes/studentRouter.js';
+import productRouter from './routes/productRouter.js';
+import userRouter from './routes/userRoute.js';
+
+let app = express();
+
+app.use(bodyParser.json());
+
+app.use(
+    (req,res,next)=>{
+        const tokenString = req.header("Authorization")
+        if(tokenString != null){
+            const token = tokenString.replace("Bearer ", "")
+            
+            jwt.verify(token, "hasi-@2000",
+                (err,decoded)=>{
+                    if(decoded != null){
+                        console.log(decoded);
+                        req.user = decoded
+                        next()
+                    }else{
+                        console.log("Invalid token");
+                        res.status(403).json({
+                            message: "Invalid token"
+                        })
+                    }
+                }
+            )
+        }else{
+            next()
+        }
+})
+
+
+
+    mongoose.connect('mongodb+srv://admin:123@cluster0.m0fa4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').
+    then(()=> {
+        console.log('Connected to MongoDB');
+    }).catch(()=>{
+        console.log('Error connecting to MongoDB'); 
+    })
+
+    app.use('/students', studentRouter)
+
+    app.use('/products', productRouter)
+
+    app.use('/users', userRouter)
+
+    app.listen(5000, () => {
+        console.log('Server is running on port 5000');
+        });
+    //mongodb+srv://admin:123@cluster0.m0fa4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+    //mongodb+srv://admin:123@cluster0.m0fa4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
---
+++ routes/productRouter.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { deleteProduct, getProducts, saveProducts } from '../controllers/productController.js';
+
+const productRouter = express.Router()
+
+productRouter.get("/", getProducts)
+
+productRouter.post("/", saveProducts)
+
+productRouter.delete("/:productId", deleteProduct)
+
+export default productRouter;
